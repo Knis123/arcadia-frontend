2e441a0f4bb3b7775ccf4f0f0a0a169b71861f79
unknown <knismarwa500@gmail.com>
2025-01-27 11:41:03 +0100

    URL backend ajouté

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://zoo-api-2ivv.onrender.com/api/v1/',
+  baseURL: 'https://arcadia-backend.onrender.com/api/v1/',
 });
 // const api = axios.create({
 //   baseURL: 'http://localhost:5000/api/v1/',
